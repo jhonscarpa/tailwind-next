ef6a8f035667c9ab79c09cf11bba6eeee0338159
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-09-10 22:25:39 -0300

    estilização com before hover e disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
     <div className="p-4 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold  text-5xl">Hello world</h1>
+      <h1 className="font-bold  text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Hello world
+      </h1>
       <h1>Hello world</h1>
       <h1>Hello world</h1>
+      <button className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 hover:enabled:bg-sky-600 hover:enabled:transition-colors disabled:opacity-60 disabled:cursor-not-allowed">
+        Sign in
+      </button>
     </div>
   )
 }
